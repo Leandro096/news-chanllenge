2b0d134acb21f5f4bd684e22786a92e9b0bb30da
Leandro096 <the.leo113@gmail.com>
2024-10-19 22:38:37 -0400

    Client side clean up for better understanding

--- client/src/store/user/user.action.js
+++ client/src/store/user/user.action.js
@@ -1,6 +1,8 @@
 import { createAction } from '../../utils/reducer/reducer.utils.js';
 import { USER_ACTION_TYPES } from './user.types';
 
+// Action creators
+// Set current user action creator
 export const setCurrentUser = (user) =>
     createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user);
 
@@ -10,6 +12,7 @@ export const checkUserSession = () =>
 export const checkUserSessionFailed = (errorMessage) =>
     createAction(USER_ACTION_TYPES.CHECK_USER_SESSION_FAILED, errorMessage);
 
+// Update user preferences action creators
 export const updatePreferenceStart = (preference) =>
     createAction(USER_ACTION_TYPES.UPDATE_PREFERENCE_START, preference);
 
@@ -19,6 +22,7 @@ export const updatePreferenceSuccess = (preference) =>
 export const updatePreferenceFailed = (errorMessage) =>
     createAction(USER_ACTION_TYPES.UPDATE_PREFERENCE_FAILED, errorMessage);
 
+// Sign in action creators
 export const emailSignInStart = (credentials) =>
     createAction(USER_ACTION_TYPES.EMAIL_SIGN_IN_START, credentials);
 
@@ -28,6 +32,7 @@ export const signInSuccess = (user) =>
 export const signInFailed = (errorMessage) =>
     createAction(USER_ACTION_TYPES.SIGN_IN_FAILED, errorMessage);
 
+// Sign up action creators
 export const signUpStart = ({ email, password, name }) =>
     createAction(USER_ACTION_TYPES.SIGN_UP_START, { email, password, name });
 
@@ -37,6 +42,7 @@ export const signUpSuccess = ({ email, password, name, language }) =>
 export const signUpFailed = (errorMessage) =>
     createAction(USER_ACTION_TYPES.SIGN_UP_FAILED, errorMessage);
 
+// Sign out action creators
 export const signOutStart = () =>
     createAction(USER_ACTION_TYPES.SIGN_OUT_START);
 
