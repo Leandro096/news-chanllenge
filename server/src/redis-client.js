53670f35a41a33488a62b00fa3050108ed78d121
Leandro096 <the.leo113@gmail.com>
2024-11-08 20:51:50 -0400

    Redis in the cloud
    Change everything to get Redis in the cloud

--- server/src/redis-client.js
+++ server/src/redis-client.js
@@ -1,6 +1,12 @@
 import redis from 'redis';
 
-const client = redis.createClient();
+const client = redis.createClient({
+    password: process.env.REDIS_PASSWORD,
+    socket: {
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT,
+    }
+});
 
 client.on('error', (err) => console.error('Redis error: ', err));
 client.connect();
